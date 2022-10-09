bfb7ac4d8672c09394214f053e24c1630b27410f
“seeinplays” <seeinplays@gmail.com>
2022-10-08 20:13:16 -0600

    lens login

--- packages/frontend/pages/components/Navbar.tsx
+++ packages/frontend/pages/components/Navbar.tsx
@@ -15,6 +15,8 @@ import {
 } from '@chakra-ui/react';
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { useAccount, useSignMessage } from 'wagmi';
+import { authenticate, generateChallenge } from '../../../frontend/utils';
 
 const NavLink = ({ children }: { children: ReactNode }) => (
   <Link
@@ -33,6 +35,31 @@ const NavLink = ({ children }: { children: ReactNode }) => (
 export default function Nav() {
   const { colorMode, toggleColorMode } = useColorMode();
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const { data } = useAccount();
+  const address = data?.address;
+  // const connected = !!data?.address;
+  const connected = true;
+  console.log("Connected: ", connected);
+
+  const { signMessageAsync } = useSignMessage();
+
+  const signIn = async () => {
+    try {
+      if (!connected) {
+        return alert('Please connect your wallet first');
+      }
+      const challenge = await generateChallenge(address);
+      const signature = await signMessageAsync({ message: challenge });
+      const accessToken = await authenticate(address, signature);
+      console.log({ accessToken });
+      window.sessionStorage.setItem('accessToken', accessToken);
+    } catch (error) {
+      console.error(error);
+      alert('Error signing in');
+    }
+  };
+
   return (
     <>
       <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
@@ -41,8 +68,12 @@ export default function Nav() {
 
           <Flex alignItems={'center'}>
             <Stack direction={'row'} spacing={7}>
-            {/* <WalletConnect /> */}
-            <ConnectButton />
+              {/* <WalletConnect /> */}
+              <ConnectButton />
+
+              <Button onClick={signIn} marginTop='2'>
+                Login with Lens
+              </Button>
 
               <Button onClick={toggleColorMode}>
                 {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
---
+++ packages/frontend/utils.ts
@@ -0,0 +1,79 @@
+// utils.js
+import {
+    ApolloClient,
+    ApolloLink,
+    HttpLink,
+    InMemoryCache,
+    gql
+} from '@apollo/client';
+
+const API_URL = '<https://api.lens.dev/>';
+
+// `httpLink` our gateway to the Lens GraphQL API. It lets us request for data from the API and passes it forward
+const httpLink = new HttpLink({ uri: API_URL });
+
+/* `authLink` takes care of passing on the access token along with all of our requests. We will be using session storage to store our access token. 
+ 
+The reason why we have to account for an access token is that that's what the Lens API uses to authenticate users. This is the token you'll get back when someone successfully signs in. We need to pass this token along with all the requests we made to the API that *need* authentication.
+*/
+const authLink = new ApolloLink((operation, forward) => {
+    const token = sessionStorage.getItem('accessToken');
+
+    operation.setContext({
+        headers: {
+            'x-access-token': token ? `Bearer ${token}` : '',
+        },
+    });
+
+    return forward(operation);
+});
+
+export const apolloClient = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+});
+
+
+const GET_CHALLENGE = `
+    query($request: ChallengeRequest!) {
+        challenge(request: $request) { text }
+    }
+`;
+
+export const generateChallenge = async (address) => {
+    const res = await apolloClient.query({
+        query: gql(GET_CHALLENGE),
+        variables: {
+            request: {
+                address,
+            }
+        }
+    });
+    return res.data.challenge.text;
+}
+
+// ...
+
+const AUTHENTICATION = `
+  mutation($request: SignedAuthChallenge!) {
+    authenticate(request: $request) {
+      accessToken
+      refreshToken
+    }
+ }
+`;
+
+export const authenticate = async (address, signature) => {
+    const { data } = await apolloClient.mutate({
+        mutation: gql(AUTHENTICATION),
+        variables: {
+            request: {
+                address,
+                signature,
+            },
+        },
+    });
+    return data.authenticate.accessToken;
+};
+
+
