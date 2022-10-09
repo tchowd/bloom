c07c7d0ff8bb1bf6913292836414a2ceec8b3fd0
Turja Chowdhury <42366823+turja-c@users.noreply.github.com>
2022-10-08 23:13:16 -0500

    Update WalletConnect.tsx

--- packages/frontend/pages/Auth/WalletConnect/pages/WalletConnect.tsx
+++ packages/frontend/pages/Auth/WalletConnect/pages/WalletConnect.tsx
@@ -28,18 +28,8 @@ export default function WalletConnect() {
 
   return isConnected ? (
     <>
-      <Link >
-      <Link
-        px={2}
-        py={1}
-        rounded={'md'}
-        _hover={{
-          textDecoration: 'none',
-          bg: useColorModeValue('gray.200', 'gray.700'),
-        }}
-        href={'#'}>
-        {children}
-      </Link>
+      <Link href="/">
+      
         <Dashboard />
       </Link>
     </>
