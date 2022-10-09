f0db542bc6cb6b85298a0d19730e2932d913f6d7
Turja Chowdhury <42366823+turja-c@users.noreply.github.com>
2022-10-09 08:38:40 -0500

    Update Discussion.tsx

--- packages/frontend/pages/Partners/Polygon/Discussion.tsx
+++ packages/frontend/pages/Partners/Polygon/Discussion.tsx
@@ -64,14 +64,13 @@ function Discussion() {
             <BlogTags tags={['Web3', 'Product Review']} marginTop="3" />
             <Heading fontSize="xl" marginTop="2">
               <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
-                Let&apos;s talk Web 3: NEAR Protocol 
+              I'm trying to develop DApp on polygon blockchain. Basically I'm minting NFT but after few mints I get this error. It even happens when I don't mint NFT. Here is my code. 
               </Link>
             </Heading>
             <Text as="p" fontSize="md" marginTop="2">
-            Imagine a blockchain that is designed for developers, capable of 
-            supporting decentralized applications (dApps) through an entirely 
-            revolutionized system and higher compared to the impact of AWS in 
-            the blockchain space. Did I also forget to mention that this blockchain...
+            I'm trying to develop DApp on polygon blockchain. Basically I'm minting NFT
+            but after few mints I get this error. It even happens when I don't mint NFT. 
+            Here is my code...
             </Text>
             <Button height={'2rem'} marginTop={'1rem'}>Read more</Button>
           </Box>
@@ -96,14 +95,14 @@ function Discussion() {
             <BlogTags tags={['Web3', 'Product Review']} marginTop="3" />
             <Heading fontSize="xl" marginTop="2">
               <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
-                Let&apos;s talk Web 3: NEAR Protocol 
+              After updating to v0.6.0 I am frequently seeing thefollowing logs. 
+              As this process is started no new blocks are minted. Any information on this would be helpful.
               </Link>
             </Heading>
             <Text as="p" fontSize="md" marginTop="2">
-            Imagine a blockchain that is designed for developers, capable of 
-            supporting decentralized applications (dApps) through an entirely 
-            revolutionized system and higher compared to the impact of AWS in 
-            the blockchain space. Did I also forget to mention that this blockchain...
+            Here is the information that I continue to recieve:
+
+            0|node1 | 2022-09-29T06:14:05.062Z [INFO] polygon.blockchain: block already inserted: block=170997 source=syncer0|node1 | 2022-09-29T06:14:17.054Z [INFO] polygon.ibft.consensus: round timeout expired: round=00|node1 | 2022-09-29T06:14:17.055Z [INFO] polygon.ibft.consensus: round started: round=10|node1 | 2022-09-29T06:14:39.057Z [INFO] polygon.ibft.consensus: round timeout expired: round=10|node1 | 2022-09-29T06:14:39.059Z [INFO] polygon.ibft.consensus: round started: round=20|node1 | 2022-09-29T06:15:21.060Z [INFO] polygon.ibft.consensus: round timeout expired: round=20|node1 | 2022-09-29T06:15:21.062Z [INFO] polygon.ibft.consensus: round started: round=30|node1 | 2022-09-29T06:16:43.064Z [INFO] polygon.ibft.consensus: round timeout expired: round=30|node1 | 2022-09-29T06:16:43.067Z [INFO] polygon.ibft.consensus: round started: round=40|node1 | 2022-09-29T06:16:43.068Z [INFO] polygon.ibft.consensus: we are the proposer
             </Text>
             <Button height={'2rem'} marginTop={'1rem'}>Read more</Button>
           </Box>
@@ -111,36 +110,6 @@ function Discussion() {
       </Wrap>
       </Box>
 
-      {/* Question 3 */}
-      <Box 
-          padding={'1.2rem'}
-          position={'relative'}
-          rounded={'2xl'}
-          borderWidth='0.2rem'
-          borderColor='gray.200'
-          _hover={{boxShadow: 'lg'}}
-          marginTop={'1rem'}
-        >
-      <Wrap spacing="30px" >
-      <WrapItem width={{ base: '100%', sm: '90%' }}>
-          <Box w="100%">
-            <BlogTags tags={['Web3', 'Product Review']} marginTop="3" />
-            <Heading fontSize="xl" marginTop="2">
-              <Link textDecoration="none" _hover={{ textDecoration: 'none' }}>
-                Let&apos;s talk Web 3: NEAR Protocol 
-              </Link>
-            </Heading>
-            <Text as="p" fontSize="md" marginTop="2">
-            Imagine a blockchain that is designed for developers, capable of 
-            supporting decentralized applications (dApps) through an entirely 
-            revolutionized system and higher compared to the impact of AWS in 
-            the blockchain space. Did I also forget to mention that this blockchain...
-            </Text>
-            <Button height={'2rem'} marginTop={'1rem'}>Read more</Button>
-          </Box>
-        </WrapItem>
-      </Wrap>
-      </Box>
     </div>
   )
 }
