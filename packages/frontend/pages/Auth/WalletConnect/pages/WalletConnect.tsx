9802f8e1dd348fd59dff336a3f563c8b71e86042
Turja Chowdhury <42366823+turja-c@users.noreply.github.com>
2022-10-08 23:08:32 -0500

    Wallet stuff - core

--- packages/frontend/pages/Auth/WalletConnect/pages/WalletConnect.tsx
+++ packages/frontend/pages/Auth/WalletConnect/pages/WalletConnect.tsx
@@ -1,24 +1,47 @@
+import Dashboard from '@/pages/Dashboard'
 import { ConnectButton, useAccount } from '@web3modal/react'
-import AccountSection from '../sections/AccountSection'
-import BalanceSection from '../sections/BalanceSection'
-import EnsSection from '../sections/EnsSection'
-import SignMessageSection from '../sections/SignMessageSection'
-import SignTypedDataSection from '../sections/SignTypedDataSection'
-import SwitchChainSection from '../sections/SwitchChainSection'
-import { TransactionSection } from '../sections/TransactionSection'
+import UseAccount from '../sections/UseAccount'
+import UseBalance from '../sections/UseBalance'
+import UseBlockNumber from '../sections/UseBlockNumber'
+import UseContract from '../sections/UseContract'
+import UseContractEvent from '../sections/UseContractEvent'
+import UseContractRead from '../sections/UseContractRead'
+import UseContractWrite from '../sections/UseContractWrite'
+import UseDisconnect from '../sections/UseDisconnect'
+import UseEnsAddress from '../sections/UseEnsAddress'
+import UseEnsAvatar from '../sections/UseEnsAvatar'
+import UseEnsName from '../sections/UseEnsName'
+import UseEnsResolver from '../sections/UseEnsResolver'
+import UseFeeData from '../sections/UseFeeData'
+import UseNetwork from '../sections/UseNetwork'
+import UseProvider from '../sections/UseProvider'
+import UsePrepareSendWaitTransaction from '../sections/UseSendTransaction'
+import UseSigner from '../sections/UseSigner'
+import UseSignMessage from '../sections/UseSignMessage'
+import UseSignTypedData from '../sections/UseSignTypedData'
+import UseSwitchNetwork from '../sections/UseSwitchNetwork'
+import UseToken from '../sections/UseToken'
+import UseTransaction from '../sections/UseTransaction'
 
 export default function WalletConnect() {
-  const { connected } = useAccount()
+  const { isConnected } = useAccount()
 
-  return connected ? (
+  return isConnected ? (
     <>
-      <AccountSection />
-      <SwitchChainSection />
-      <SignTypedDataSection />
-      <SignMessageSection />
-      <BalanceSection />
-      <TransactionSection />
-      <EnsSection />
+      <Link >
+      <Link
+        px={2}
+        py={1}
+        rounded={'md'}
+        _hover={{
+          textDecoration: 'none',
+          bg: useColorModeValue('gray.200', 'gray.700'),
+        }}
+        href={'#'}>
+        {children}
+      </Link>
+        <Dashboard />
+      </Link>
     </>
   ) : (
     <ConnectButton />
--- packages/frontend/pages/components/Navbar.tsx
+++ packages/frontend/pages/components/Navbar.tsx
@@ -12,6 +12,7 @@ import {
   Stack,
   useColorMode,
   Center,
+  Image,
 } from '@chakra-ui/react';
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
@@ -62,11 +63,16 @@ export default function Nav() {
 
   return (
     <>
-      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+      <Box bg={useColorModeValue('purple.100', 'purple.900')} px={4}>
         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <Box>Logo</Box>
-
-          <Flex alignItems={'center'}>
+            <Image
+                borderRadius="lg"
+                src='/static/bloom.png'
+                alt="some good alt text"
+                objectFit="contain"
+                width={'10rem'}
+              />          
+              <Flex alignItems={'center'}>
             <Stack direction={'row'} spacing={7}>
               {/* <WalletConnect /> */}
               <ConnectButton />
